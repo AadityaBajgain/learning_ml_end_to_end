674cd28ecc7bc4bcd991db2ed0834b0d854003b6
Aaditya Bajgain <adityabajgain@gmail.com>
2026-02-16 00:03:40 -0800

    completed data_ingestion and created artifacts - (raw data, train and test data)

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -27,5 +27,23 @@ class DataIngestion:
         try:
             df = pd.read_csv("notebooks/data/stud.csv")
             logging.info("Read the dataset as dataframe")
-        except:
-            pass
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            
+            train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+            
+            
+            logging.info("Ingestion of the data is completed")
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+        
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initialize_data_ingestion()
